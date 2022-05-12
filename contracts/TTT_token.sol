ac9046d7a409d863b0a9f28e2dac60c5cfe029c1
Nikita Tsigelnikov <nikita_tsigelnikov@mail.ru>
2022-05-12 23:51:58 +0300

    git init

---
+++ contracts/TTT_token.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TTT is ERC20 {
+
+}
