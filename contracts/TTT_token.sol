f71b6a3e3de6d495fc331f37b593b0b17803a925
Nikita Tsigelnikov <nikita_tsigelnikov@mail.ru>
2022-05-13 05:06:43 +0300

    added white list, methods, modifiers and events for this

--- contracts/TTT_token.sol
+++ contracts/TTT_token.sol
@@ -9,11 +9,71 @@ contract TTT is ERC20, Ownable {
     /*
         Only user fron white list can call transfer before ICO ending
     */
-
-    mapping (address => uint8) whiteList;
+    mapping (address => bool) whiteList;
+    address ICOAddress;
     uint immutable ICO_END_TIME;
 
+    /*
+        modofiers
+    */
+    modifier isICO() {
+        require(msg.sender == ICOAddress,
+            "This method can call only ICO contract");
+        _;
+    }
+
+    modifier ICOInProccessing() {
+        require(block.timestamp > ICO_END_TIME,
+            "ICO is done");
+        _;
+    }
+
+    modifier isWhiteListParticipant(address participant) {
+        require(whiteList[participant] == true,
+            "You are not in white list");
+        _;
+    }
+
+    /*
+        events
+    */
+    event removedFromWhiteList(address indexed participant);
+    event addedToWhiteList(address indexed participant);
+ 
     constructor(uint ICO_endTIme) ERC20("TTT", "TTT") {
         ICO_END_TIME = ICO_endTIme;
+    }
+
+    /*
+        Use this function ONLY while ICO.  
+    */
+    function buyTokens(address reciever, uint amount)
+        external
+        isICO
+        ICOInProccessing
+    {
+        _mint(reciever, amount);
+    }
+
+    /*
+        Addresses from whilte list have additional privilegies.
+        (For example call trasnfer before the ICO end)
+    */
+    function addToWhiteList(address participant)
+        external
+        onlyOwner
+    {
+        require(participant != address(0), "Participant address is zero");
+        whiteList[participant] = true;
+        emit addedToWhiteList(participant);
+    }
+
+    function removeFromWhiteList(address participant)
+        external
+        onlyOwner
+    {
+        require(participant != address(0), "Participant address is zero");
+        whiteList[participant] = false;
+        emit removedFromWhiteList(participant);
     }
 }
