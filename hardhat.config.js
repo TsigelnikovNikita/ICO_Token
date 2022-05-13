67ed47a2394fa48cc2eaade700c8c023af9cae80
Nikita Tsigelnikov <nikita_tsigelnikov@mail.ru>
2022-05-13 21:48:59 +0300

    added hardhat-etherscan package

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require('hardhat-dependency-compiler');
 require('solidity-coverage');
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 require('./tasks/ICO_buy');
@@ -14,6 +15,15 @@ require('./tasks/Token_balanceOf');
  */
 module.exports = {
   solidity: "0.8.4",
+  etherscan: {
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`
+  },
+  networks: {
+    rinkeby: {
+      url: `${process.env.RINKEBY_NETWORK_URI}`,
+      accounts: [`${process.env.SIGNER_PRIVATE_KEY}`]
+    }
+  },
   dependencyCompiler: {
     paths: [
       "@openzeppelin/contracts/token/ERC20/ERC20.sol",
